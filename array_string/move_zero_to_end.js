bcd902e6f31697a98f784b70116382b83b499f49
Ashish Mishra <69194675+ashish3010@users.noreply.github.com>
2025-04-14 17:26:29 +0530

    Update move_zero_to_end.js

--- array_string/move_zero_to_end.js
+++ array_string/move_zero_to_end.js
@@ -3,6 +3,22 @@
 const arr1 = [0, 1, 0, 3, 12]
 
 const moveZeroToEnd = (arr) => {
+  const res = []
+  const zeroRes = []
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      res.push(arr[i])
+    } else {
+      zeroRes.push(arr[i])
+    }
+  }
+
+  return [...res, ...zeroRes]
+}
+
+// optimal solution
+const moveZeroToEnd = (arr) => {
   let i = 0
   for (let j = 0; j < arr.length; j++) {
     if (arr[j] !== 0) {
