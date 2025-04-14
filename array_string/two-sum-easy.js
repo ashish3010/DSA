fce64ab417f23ab6205088e235310dd315593f2f
Ashish Mishra <69194675+ashish3010@users.noreply.github.com>
2025-04-14 12:52:42 +0530

    Update two-sum-easy.js

--- array_string/two-sum-easy.js
+++ array_string/two-sum-easy.js
@@ -5,17 +5,17 @@ const twoSumEasy = (arr, target) => {
   const obj = {}
   let res = []
 
-  for (let i = 0; i < arr1.length; i++) {
-    if (obj[arr1[i]]) {
+  for (let i = 0; i < arr.length; i++) {
+    if (obj[arr[i]]) {
       continue
     } else {
-      obj[arr1[i]] = i
+      obj[arr[i]] = i
     }
   }
 
-  for (let i = 0; i < arr1.length; i++) {
-    if (obj[k - arr[i]]) {
-      res = [i, obj[k - arr[i]]]
+  for (let i = 0; i < arr.length; i++) {
+    if (obj[target - arr[i]]) {
+      res = [i, obj[target - arr[i]]]
       break
     } else {
       continue
