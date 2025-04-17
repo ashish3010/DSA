115fffd460eaca94791cb297d1a9d6bcfee91d00
Ashish Mishra <69194675+ashish3010@users.noreply.github.com>
2025-04-17 15:50:06 +0530

    Update check_anagram.js

--- array_string/check_anagram.js
+++ array_string/check_anagram.js
@@ -1,20 +1,37 @@
-const str1 = "Hello"
-const str2 = "lloeh"
+const s = "aab"
+const t = "bba"
 
-const checkAnagram = (string1, string2) => {
-  const str1 = string1.toLowerCase()
-  const str2 = string2.toLowerCase()
-
-  let res = false
+const checkAnagram = (str1, str2) => {
   if (str1.length !== str2.length) {
     return false
   }
+  let res = false
+  const obj = {}
+  const obj2 = {}
   for (let i of str1) {
-    if (str2.includes(i)) {
-      res = true
+    if (obj[i]) {
+      obj[i]++
     } else {
-      return false
+      obj[i] = 1
     }
   }
+
+  for (let i of str2) {
+    if (obj2[i]) {
+      obj2[i]++
+    } else {
+      obj2[i] = 1
+    }
+  }
+
+  for (let i of str2) {
+    if (obj2[i] !== obj[i]) {
+      res = false
+      break
+    } else {
+      res = true
+    }
+  }
+  
   return res
 }
