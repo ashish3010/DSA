21e25575cbfcfcd14721fb47c7d635598153fd59
Ashish Mishra <69194675+ashish3010@users.noreply.github.com>
2025-06-10 23:21:29 +0530

    Create using-linked-list.js

---
+++ Queue/using-linked-list.js
@@ -0,0 +1,41 @@
+class Node {
+  constructor(value){
+    this.value=value,
+    this.next=null
+  }
+}
+
+class Queue {
+  constructor(){
+    this.first = null
+    this.last = null
+    this.length = 0
+  }
+  peek(){
+    return this.first
+  }
+  enqueue(value){
+    const newNode = new Node(value)
+    if(this.length === 0){
+      this.first = newNode
+      this.last = newNode
+    }else{
+      this.last.next = newNode
+      this.last = newNode
+    }
+      this.length++
+  }
+  dequeue(){
+    if(this.length === 0){
+      return null
+    }
+    if(this.length === 1){
+      this.last = null
+    }
+    this.first = this.first.next
+    this.length--
+  }
+}
+
+const myQueue = new Queue()
+console.log(myQueue)
